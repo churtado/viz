f00ed9ee40af46cac14d65a9d5c3c9d14943550b
charliepapa2@gmail.com <charliepapa2@gmail.com>
2017-07-03 22:01:27 +0100

    some changes

--- src/app/mock-vizzes.ts
+++ src/app/mock-vizzes.ts
@@ -1,11 +1,9 @@
 import { Viz } from './viz';
 
 export const VIZZES: Viz[] = [
-    { id: 1, name: 'bar chart 1', description: 'a bar chart 1' }, 
-    { id: 2, name: 'bar chart 2', description: 'another bar chart' },
-    { id: 3, name: 'bar chart 3', description: 'another bar chart' },
-    { id: 4, name: 'bar chart 4', description: 'another bar chart' },
-    { id: 5, name: 'bar chart 5', description: 'another bar chart' },
-    { id: 6, name: 'bar chart 6', description: 'another bar chart' },
-    { id: 7, name: 'bar chart 7', description: 'another bar chart' },
+    { 
+        id: 1, 
+        name: 'Simple Bar Chart',
+        description: 'A simple bar chart in vega'
+    },
 ];
--- src/app/viz.ts
+++ src/app/viz.ts
@@ -2,4 +2,5 @@ export class Viz {
   id: number;
   name: string;
   description: string;
+  specUrl: './spec/simple_bar.vg.json'
 }
