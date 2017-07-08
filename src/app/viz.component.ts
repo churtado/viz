f1a87c484a76bedcc404305a019096ccd85dd94b
charliepapa2@gmail.com <charliepapa2@gmail.com>
2017-07-08 12:01:29 +0100

    added tooltips to vizzes

--- src/app/viz.component.ts
+++ src/app/viz.component.ts
@@ -7,6 +7,7 @@ import { Viz } from './viz';
 
 declare function require(name: string);
 const vega = require('vega');
+const vegaTooltip = require('vega-tooltip');
 
 @Component({
     selector: 'app-viz',
@@ -32,12 +33,21 @@ export class VizComponent implements OnInit {
                 this.viz = viz;
                 // render vega component
                 this.vizService.getVegaSpec(this.viz.specUrl).subscribe((json) => {
+
+                    // create runtime dataflow of the viz
+                    const runtime = vega.parse(json);
                     // render the viz
-                    const view = new vega.View(vega.parse(json))
+                    const view = new vega.View(runtime)
                         .renderer('svg')  // set renderer (canvas or svg)
                         .initialize('#view') // initialize view within parent DOM container
                         .hover()             // enable hover encode set processing
                         .run();              // run the dataflow and render the view
+
+                    // options for vega tooltip
+                    const options = {
+                        mode: 'vega'
+                    }
+                    vegaTooltip.vega(view);
                 })
             });
     }
