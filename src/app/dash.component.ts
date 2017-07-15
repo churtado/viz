1dc6241be54ceb1f05c53c60b251d5ef36f7ee2d
charliepapa2@gmail.com <charliepapa2@gmail.com>
2017-07-15 13:25:32 +0100

    dashboard model loads

--- src/app/dash.component.ts
+++ src/app/dash.component.ts
@@ -1,16 +1,30 @@
 import 'rxjs/add/operator/switchMap';
 import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+
+import { VizService } from './viz.service';
+
+import { Dash } from './dash';
 
 @Component({
     selector: 'app-dash',
     templateUrl: './dash.component.html',
+    styleUrls: ['./dash.component.css'],
 })
 export class DashComponent implements OnInit {
+    @Input() dash: Dash;
 
     constructor(
+        private vizService: VizService,
+        private route: ActivatedRoute,
     ) { }
 
     ngOnInit(): void {
+        this.route.paramMap
+        .switchMap((params: ParamMap) => this.vizService.getDash(+params.get('id')))
+        .subscribe((dash: Dash) => {
+            this.dash = dash;
+        });
     }
 
 }
